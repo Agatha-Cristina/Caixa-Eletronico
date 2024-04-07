dae8aed0e4b9e2721df7dd1083dfee1e3a9fefab
Agatha <agatha.cristina0507@gmail.com>
2024-04-07 12:01:09 -0300

    feat: Initial configuration

---
+++ server.js
@@ -0,0 +1,6 @@
+import { createServer } from 'node:http'
+const server = createServer(() => {
+    console.log('oi')
+})
+
+server.listen(3333)
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { server } from './server/server';
+
+server.listen(7777, () => console.log("App rodando!"));
